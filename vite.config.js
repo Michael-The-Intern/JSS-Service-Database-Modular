cd3654f9748dcadfed70407bf3556f1bcfff2183
Michael-The-Intern <michael.colon@joysonsafety.com>
2026-07-13 10:36:16 -0400

    Vite and ActionQueues changes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/JSS-Service-Database-Modular/',
   plugins: [react()],
+  base: '/JSS-Service-Database-Modular/',
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
   build: {
     outDir: 'dist',
+    sourcemap: true,
   },
 });
